d6f8da9cd428d9cb3027544a095b21256d6f8953
Sachin kumar prasad <skpkorba9009@gmail.com>
2023-07-22 13:43:32 +0530

    commot all

--- event-bus/index.js
+++ event-bus/index.js
@@ -10,18 +10,38 @@ app.post('/events', (req, res) => {
     try {
         const event = req.body
         events.push(event)
-        axios.post('http://localhost:4000/events', event).catch((err) => {
-            console.log(err.message)
-        })
-        axios.post('http://localhost:4001/events', event).catch((err) => {
-            console.log(err.message)
-        })
-        axios.post('http://localhost:4002/events', event).catch((err) => {
-            console.log(err.message)
-        })
-        axios.post('http://localhost:4003/events', event).catch((err) => {
-            console.log(err.message)
-        })
+        axios
+            .post('http://posts-clusterip-srv:4000/events', event)
+            .then(() => {
+                console.log('Post API called successfull')
+            })
+            .catch((err) => {
+                console.log(err.message)
+            })
+        axios
+            .post('http://comments-clusterip-srv:4001/events', event)
+            .then(() => {
+                console.log('Comment API called successfull')
+            })
+            .catch((err) => {
+                console.log(err.message)
+            })
+        axios
+            .post('http://query-clusterip-srv:4002/events', event)
+            .then(() => {
+                console.log('Query API called successfull')
+            })
+            .catch((err) => {
+                console.log(err.message)
+            })
+        axios
+            .post('http://moderation-clusterip-srv:4003/events', event)
+            .then(() => {
+                console.log('Moderation API called successfull')
+            })
+            .catch((err) => {
+                console.log(err.message)
+            })
 
         res.send({ status: 'Ok' })
     } catch (error) {
